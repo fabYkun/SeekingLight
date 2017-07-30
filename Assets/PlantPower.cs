0e3bf199f10d3ad6ac5cc6cc6520cfb387d058d1
fabYkun <faby.borel@gmail.com>
2017-07-30 13:03:34 +0200

    la plante se dandine

--- Assets/PlantPower.cs
+++ Assets/PlantPower.cs
@@ -10,6 +10,9 @@ public class                                        PlantPower : MonoBehaviour
     public int                                      size;
     public float                                    distance = 0.4f;
     public float                                    elasticity = 1;
+    public int                                      controlledNodes = 1; // number of nodes you actually control
+    public AnimationCurve                           powerDistribution; // force given to manual nodes in descending order
+    public float                                    maxPower; // maximal power
 
     void                                            Awake()
     {
@@ -30,6 +33,7 @@ public class                                        PlantPower : MonoBehaviour
         tmp.transform.up = lastJoint.transform.up;
         currentJoint = tmp.GetComponentInChildren<HingeJoint2D>();
         currentJoint.connectedBody = lastJoint.GetComponent<Rigidbody2D>();
+        ++controlledNodes; // maybe not
         this.joints.Add(currentJoint);
     }
 
@@ -37,6 +41,17 @@ public class                                        PlantPower : MonoBehaviour
     {
         if (Input.GetButtonDown("Spawn"))
             this.AddNode();
-        
+        if (Input.GetButtonDown("Left") || Input.GetButtonDown("Right"))
+        {
+            for (int i = 0; i < controlledNodes; ++i)
+            {
+                HingeJoint2D joint = this.joints[this.joints.Count - (i + 1)];
+                JointMotor2D motor = joint.motor;
+                motor.motorSpeed = this.powerDistribution.Evaluate((float) i / controlledNodes) * this.maxPower;
+                if (Input.GetButtonDown("Left")) motor.motorSpeed = -motor.motorSpeed;
+                joint.motor = motor;
+                
+            }
+        }
     }
 }
