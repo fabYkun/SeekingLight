a54d80a2e10651dc59e5baf346a0841fd6c76806
Hugo Valery <hugo.valery@gmail.com>
2017-07-30 17:26:27 +0200

    light goes away, tweaked reproduction, worked on scene

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver: MonoBehaviour {
+    void OnTriggerEnter2D (Collider2D other) {
+        if(other.gameObject.tag == "gameOver")
+        print("GAMEOVER BECAUSE OF LIGHT");
+
+        }
+    }
---
+++ Assets/Scripts/LightScroll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScroll : MonoBehaviour {
+    public AnimationCurve scrollSpeed;
+    float timer = 0; // correspond au x de la courbe precedante
+	public float maxtimer, unitPerSec;
+
+	void Update () {
+		timer += Time.deltaTime;
+        transform.position = new Vector3(transform.position.x + Time.deltaTime * unitPerSec, transform.position.y, transform.position.z);
+	}
+}
