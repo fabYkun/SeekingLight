b8283d981d642c27d24ea0fdcb886a6c70ccc868
fabYkun <faby.borel@gmail.com>
2017-07-30 10:20:04 +0200

    init

---
+++ Assets/PlantPower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class                                        PlantPower : MonoBehaviour
+{
+    public List<HingeJoint2D>                       joints = new List<HingeJoint2D>();
+    public int                                      size;
+
+    void                                            Awake()
+    {
+        
+    }
+}
