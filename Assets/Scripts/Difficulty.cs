d2fef5f680e898b2b2d2c51375174a207c8e5d79
borel_d <fabien.borel@epitech.eu>
2017-08-01 00:06:32 +0200

    difficulty menu

---
+++ Assets/Scripts/Difficulty.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class                        Difficulty : MonoBehaviour
+{
+	void                            Awake()
+    {
+        Time.timeScale = 0;
+    }
+	
+	public void                     Easy()
+    {
+        GameOver.instance.difficultyMode = DifficultyEnum.Easy;
+        Time.timeScale = 1;
+        this.gameObject.SetActive(false);
+    }
+
+    public void                     Normal()
+    {
+        GameOver.instance.difficultyMode = DifficultyEnum.Normal;
+        Time.timeScale = 1;
+        this.gameObject.SetActive(false);
+    }
+
+    public void                     Difficult()
+    {
+        GameOver.instance.difficultyMode = DifficultyEnum.Hard;
+        Time.timeScale = 1;
+        this.gameObject.SetActive(false);
+    }
+}
