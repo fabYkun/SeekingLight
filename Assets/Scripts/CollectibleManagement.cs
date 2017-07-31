67fb0e1bac655c70d5308faa8c4d8e180d250a30
borel_d <fabien.borel@epitech.eu>
2017-07-31 16:37:55 +0200

    collectibles alright

---
+++ Assets/Scripts/CollectibleManagement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class                    CollectibleManagement : MonoBehaviour
+{
+    public Image                gauge;
+
+    public void                 SetGauge (float perc)
+    {
+        this.gauge.fillAmount = perc / 100;
+    }
+
+    public float                GetValue()
+    {
+        return this.gauge.fillAmount * 100;
+    }
+}
--- Assets/Scripts/Seed.cs
+++ Assets/Scripts/Seed.cs
@@ -2,65 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Seed: MonoBehaviour {
+public class                Seed: MonoBehaviour
+{
     Rigidbody2D             rb2d;
     public float            thrust;
     public float            rotationSpeed, pollenPerSec, pollenMax=6;
     private float           pollenReserve;
+    private float           waterReserve;
     public AnimationCurve   powerPerc;
-    PollenManagement        pM;
-    WaterManagement         wM;
+    CollectibleManagement   pollenGauge;
+    CollectibleManagement   waterGauge;
 
     PlantPower              pP;
     Camera                  cam;
     public GameObject       bebePlante;
 
-    void Start ( ) {
-        pM = FindObjectOfType<PollenManagement> ( );
-        wM = FindObjectOfType<WaterManagement> ( );
-
+    void                    Start()
+    {
         rb2d = gameObject.GetComponent<Rigidbody2D> ( );
         cam = FindObjectOfType<Camera>();
-        }
+    }
 
-    void Update ( ) {
-        if ( pollenReserve > 0 )
+    void                    Update()
+    {
+        if (pollenReserve > 0)
             Propulsion ( );
         Rotation ( );
         cam.Follow(gameObject.transform);
-        }
+    }
 
-    void Propulsion ( ) {
-        bool input = Input.GetButton ("Propulse");
+    void                    Propulsion()
+    {
+        bool                input = Input.GetButton ("Propulse");
 
-        if ( input ) {
-            Vector2 adaptedForce = transform.up * powerPerc.Evaluate (pollenReserve/ pollenMax) * thrust;
+        if (input)
+        {
+            Vector2         adaptedForce = transform.up * powerPerc.Evaluate (pollenReserve/ pollenMax) * thrust;
             //print (adaptedForce);
             rb2d.AddForce (adaptedForce);
             pollenReserve -= Time.deltaTime * pollenPerSec;
-            }
-        pM.SetPollenGauge ((int)pollenReserve);
         }
+        pollenGauge.SetGauge ((int)pollenReserve);
+    }
 
-    void Rotation ( ) {
+    void                    Rotation()
+    {
         if ( Input.GetButton ("Left") )
             rb2d.AddTorque (rotationSpeed);
         if ( Input.GetButton ("Right") )
             rb2d.AddTorque (-rotationSpeed);
-        }
+    }
 
-    public void Initialize (PlantPower plPo) {
+    public void             Initialize(PlantPower plPo)
+    {
         this.pP = plPo;
+        this.pollenGauge = plPo.pollenGauge;
+        this.waterGauge = plPo.waterGauge;
         this.pollenReserve = plPo.pollenReserve;
-        }
+        this.waterReserve = plPo.waterReserve;
+    }
 
-    void OnCollisionEnter2D(Collision2D col) {
-        if(col.gameObject.tag == "terre") {
+    void                    OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "terre")
+        {
             print("entered");
             GameObject tmp = Instantiate(bebePlante, transform.position, bebePlante.transform.rotation);
-            tmp.GetComponent<PlantPower>().Initialize(wM, pM);
+            tmp.GetComponent<PlantPower>().Initialize(this.waterGauge, this.pollenGauge);
             DestroyObject(gameObject);
-            }
-        else print("GAMEOVER BECAUSE OF PAS FERTILE");
         }
+        else print("GAMEOVER BECAUSE OF PAS FERTILE");
     }
+}
