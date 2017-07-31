92a1f934e1c709c8395966a20dea02fd680c2119
Hugo Valery <hugo.valery@gmail.com>
2017-08-01 01:09:51 +0200

    tweeks for jauge progress

--- Assets/ProgressJauge.cs
+++ Assets/ProgressJauge.cs
@@ -8,6 +8,7 @@ public class ProgressJauge: MonoBehaviour {
     public Image soleilIcon, plantIcon;
     public Transform cam, sol;
     public float startJauge, endJauge, startReal, endReal;
+    public float adjustLight;
 
     // Update is called once per frame
     void Update ( ) {
@@ -22,7 +23,7 @@ public class ProgressJauge: MonoBehaviour {
         Vector3 plantPoz = new Vector3 (startJauge + jaugePercCam, plantIcon.transform.localPosition.y, plantIcon.transform.localPosition.z);
         plantIcon.transform.localPosition = plantPoz;
 
-        float pozSol = sol.position.x - startReal;
+        float pozSol = sol.position.x - startReal+ adjustLight;
         float percSol = pozSol/tmp;
                 //print ("percsol " + percSol);
 
