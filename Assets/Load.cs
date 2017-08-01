f3192ca5628caeda1dbb317f5c1843a0a200d764
Hugo Valery <hugo.valery@gmail.com>
2017-08-01 02:06:22 +0200

    last stuff ?

---
+++ Assets/Load.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load: MonoBehaviour {
+
+    public string name;
+
+    void OnTriggerEnter2D (Collider2D col) {
+        if ( col.gameObject.tag == "player" )
+            SceneManager.LoadScene (name, LoadSceneMode.Single);
+        }
+        }
